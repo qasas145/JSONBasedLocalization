14d449c8900f88f4d96ddfd1e6aed3cd799f9553
qasas145 <mohamed.sayed14527@yahoo.com>
2024-12-06 22:05:33 +0200

    jsonstringlocalizerfactory file added

---
+++ JsonStringLocalizerFactory.cs
@@ -0,0 +1,14 @@
+using Microsoft.Extensions.Localization;
+
+public class JsonStringLocalizerFactory : IStringLocalizerFactory
+{
+    public IStringLocalizer Create(Type resourceSource)
+    {
+        return new JsonStringLocalizer();
+    }
+
+    public IStringLocalizer Create(string baseName, string location)
+    {
+        return new JsonStringLocalizer();
+    }
+}
