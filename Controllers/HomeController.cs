bfb75232dd2cc99a0f76942f608e1f6bd1838bcd
qasas145 <mohamed.sayed14527@yahoo.com>
2024-12-07 06:37:04 +0200

    Resources folder has been added

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,20 +1,28 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using JsonBasedLocalization.Models;
+using Microsoft.Extensions.Localization;
 
 namespace JsonBasedLocalization.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly IStringLocalizer<HomeController> _localizer;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, IStringLocalizer<HomeController> localizer)
     {
         _logger = logger;
+        _localizer = localizer;
     }
 
     public IActionResult Index()
     {
+        var welcomeMessage = _localizer["welcome"];
+        Console.WriteLine(welcomeMessage.ResourceNotFound);
+        Console.WriteLine(welcomeMessage.Name);
+        Console.WriteLine(welcomeMessage.Value);
+        ViewBag.welcomeMessage = welcomeMessage;
         return View();
     }
 
